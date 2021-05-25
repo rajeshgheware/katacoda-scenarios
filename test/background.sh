5dcd6d67d096952efcc8410b8f839e1dce2b0e00
Rajesh G <rajesh@unigps.in>
2021-05-26 05:29:15 +0530

    test

--- test/background.sh
+++ test/background.sh
@@ -8,5 +8,7 @@ while ! nc -z node01 30080; do
   sleep 0.5
 done
 
+while [ ! -f /usr/local/bin/wait.sh ]; do sleep 1; done; sleep 1;  /usr/local/bin/wait.sh; /usr/local/bin/start.sh
+
 echo "Started"
 echo 'done' > /opt/katacoda-background-finished
