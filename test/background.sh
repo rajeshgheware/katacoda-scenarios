eb4fc41bf3de49468bf4ee3c183e7be748e3461f
Rajesh G <rajesh@unigps.in>
2021-05-26 05:57:44 +0530

    test

--- test/background.sh
+++ test/background.sh
@@ -1,6 +1,5 @@
 launch.sh
 kubectl apply -f /opt/deploy.yaml
-rm /opt/deploy.yaml
 
 echo "Waiting for 30080..."
 
@@ -11,4 +10,4 @@ done
 while [ ! -f /usr/local/bin/wait.sh ]; do sleep 1; done; sleep 1;  /usr/local/bin/wait.sh; /usr/local/bin/start.sh
 
 echo "Started"
-echo 'done' > /opt/katacoda-background-finished
+echo "done" > /opt/katacoda-background-finished
