6050067efa636e4eb0dfd3a55111035874f4cea2
Rajesh G <rajesh@unigps.in>
2021-05-26 20:08:03 +0530

    refine

--- test/foreground.sh
+++ test/foreground.sh
@@ -6,7 +6,5 @@ while ! nc -z controlplane 30080; do
   sleep 0.5
 done
 
-while [ ! -f /usr/local/bin/wait.sh ]; do sleep 1; done; sleep 1;  /usr/local/bin/wait.sh; /usr/local/bin/start.sh
-
 echo "Started"
 echo "done" 
